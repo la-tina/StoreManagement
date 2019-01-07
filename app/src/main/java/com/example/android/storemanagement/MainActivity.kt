2092500c2249a57dcd52657329282988424ebad6
Konstantina Rovelova <konstantina.rovelova@gmail.com>
2019-01-07 17:37:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/storemanagement/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.android.storemanagement
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.widget.Button
+import android.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+}
